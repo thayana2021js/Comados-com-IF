2d51cba964c6912d2797ca8e7bf111bfd4450013
Thayana Vilaça Fortunato <76934028+thayana2021js@users.noreply.github.com>
2022-02-05 19:19:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ComandosIf
+{
+ 
+   class Program
+    {
+       static void Main(string[] args) 
+       {
+        int idade;
+
+           Console.WriteLine("DIGITE A SUA IDADE: ");
+           idade = int.Parse(Console.ReadLine());
+
+           if(idade >=21)
+           {
+            Console.WriteLine("VOCÊ É MAIOR DE  IDADE ");
+            
+            }
+            else{
+            Console.WriteLine("VOCÊ É MENOR DE IDADE ");
+            }
+        }    
+    }
+    
+}
+
+
